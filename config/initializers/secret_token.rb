6de6ca305314b7b25c5aa746b64ebed9096ccba0
Michael Price <mike@quantumpolitics.org>
2012-11-01 15:32:26 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b11d3bcc9d098ab813ab37ac30eecd75648357c9694ed79494e8f3c02dd85c0dae16724de63a613af944d1de8b06666c0075db5090e627b637a551b2b74b01e4'
